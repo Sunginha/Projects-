73e4f5aa4d7f11bce63bf9aa9cc3db8617734cd0
Sunga <65925056+Sunginha@users.noreply.github.com>
2024-01-31 21:46:51 -0300

    Delete P1.py

--- P1.py
+++
@@ -1,6 +0,0 @@
-from random import randint
-print(20*"=-=")
-print("CAR GAME".center(75))
-print(20*"=-=")
-player1 = int(input("Choose a number and i will stablish a path for the car \nEnter the number:").strip())
-Console = randint(1, 10)
